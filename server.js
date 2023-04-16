9869a1032a6b0f18302ed39f2dc1d8c43c466070
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-04-16 20:03:50 +0300

    add middlewares to application

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const path = require('path')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const config = require('./config')
+
+const appLocalsStringsMiddleware = require('./middlewares/app_locals')
+const notFoundMiddleware = require('./middlewares/not_found')
+const errorMiddleware = require('./middlewares/error')
 
 // встановлюємо express
 const express = require('express')
@@ -7,9 +14,16 @@ const app = express()
 // встановлюємо директорію для віддачі статичного контенту (каталог проекту)
 app.use(express.static(__dirname))
 
+app.use(flash())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // налаштовуємо роботу із шаблонізаотором
-app.set('views', path.join(__dirname, '/static/views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+
+app.use(appLocalsStringsMiddleware)
 
 // налаштовуємо маршрутизацію
 app.get('/', function (_request, response) {
@@ -30,5 +44,11 @@ app.get('/shipInPort', function (_request, response) {
   response.render('pages/shipInPort', { title: 'ShipInPort' })
 })
 
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
 // запускаємо аплікацію
-app.listen(process.env.PORT || 8080)
+const { host, port } = config
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`)
+})
